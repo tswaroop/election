ed404a07155f99b8d9d5475665acd26e6baf93a4
S Anand <root.node@gmail.com>
2014-04-10 09:48:20 +0530

    Show 2014 candidates standing for elections based on ECI affidavits

---
+++ affidavits.py
@@ -0,0 +1,15 @@
+"""
+Rename STATE and PC in affidavit.csv based on PC.csv
+
+Usage: python affidavits.py
+"""
+
+import pandas as pd
+
+affidavits = pd.read_csv('affidavits.csv')
+pc = pd.read_csv('pc.csv').set_index(['ST_CODE', 'PC_CODE'])
+
+affidavits['STATE'] = affidavits.apply(lambda row: pc.ix[row['ST_CODE'], row['PC_CODE']]['STATE'], axis=1)
+affidavits['PC'] = affidavits.apply(lambda row: pc.ix[row['ST_CODE'], row['PC_CODE']]['PC'], axis=1)
+
+affidavits.to_csv('affidavits.csv', index=False)
