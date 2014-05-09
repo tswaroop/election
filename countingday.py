22b8e6585dadb5eab525b293445616e3735286db
S Anand <root.node@gmail.com>
2014-05-09 07:39:04 +0530

    /live/ basic version added

--- countingday.py
+++ countingday.py
@@ -9,8 +9,6 @@ Creates the following files:
 
 2014-constituency.csv:
     - CONST_ID
-    - STATE
-    - PC
     - WINNER
     - WINNER PARTY
     - WINNER VOTES
@@ -22,6 +20,7 @@ Creates the following files:
 """
 
 import json
+import datetime
 import argparse
 import pyodbc
 import pandas as pd
@@ -50,18 +49,28 @@ def simulate(candidates, time):
 def main():
     # Process command line arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument('--server', help='Server name', default='localhost')
-    parser.add_argument('--db', help='Database name', default='ELECTIONS_2014')
+    parser.add_argument('--server', help='SQL Server name', default='localhost')
+    parser.add_argument('--db', help='Database name', default='ELEC_6MAY_6_20PM')
     parser.add_argument('--time', help='Simulation real time 0.0-1.0', type=float, default=1.0)
+    parser.add_argument('--uid', help='SQL Server username')
+    parser.add_argument('--pwd', help='SQL Server username')
     args = parser.parse_args()
 
     # Connect to SQL Server
-    con = pyodbc.connect('DRIVER={SQL Server Native Client 11.0};' +
-            'SERVER={:s};'.format(args.server) +
-            'DATABASE={:s};'.format(args.db) +
-            'WSID={:s};'.format(args.server) +
-            'APP=PyCountingDay;' +
-            'Trusted_Connection=Yes')
+    dsn = ('DRIVER={:s};' +
+          'SERVER={:s};'.format(args.server) +
+          'DATABASE={:s};'.format(args.db) +
+          'WSID={:s};'.format(args.server) +
+          'APP=PyCountingDay;')
+    if not args.uid:
+        dsn += 'Trusted_Connection=Yes'
+    else:
+        dsn += 'UID={:s};PWD={:s};'.format(args.uid, args.pwd)
+
+    try:
+        con = pyodbc.connect(dsn.format('{SQL Server Native Client 11.0}'))
+    except pyodbc.Error:
+        con = pyodbc.connect(dsn.format('{SQL Server}'))
 
     # Load constituency master
     constituencies = read_sql('SELECT * FROM dbo.CONST_MST', con, coerce_float=False)
@@ -69,49 +78,69 @@ def main():
     constituencies = constituencies.set_index('CONST_ID')
 
     # Load latest candidate votes
-    candidates = read_sql('SELECT * FROM dbo.ELECA_CANDMAST', con, coerce_float=False)
+    candidates = read_sql('SELECT * FROM dbo.ELECP_CANDMAST', con, coerce_float=False)
 
     # Mimic VOTES at an earlier point in time
-    candidates = simulate(candidates, args.time)
+    if args.time < 1:
+        candidates = simulate(candidates, args.time)
 
     # Summarise by constituency
+    candidates['VOTES'] = candidates['VOTES'].fillna(0).astype(int)
+    candidates['ID'] = candidates['CCODE'].astype(str).apply(lambda v: 'S' + v[:2] + '-' + v[-2:])
     const_wise = candidates.groupby('CCODE')
     candidates['#'] = const_wise['VOTES'].transform(lambda v: v.rank(ascending=False, method='first'))
-    candidates['TIME'] = const_wise['CANDI_UPDATE_TIME'].transform('max')
-    winners = candidates[candidates['#'] == 1].set_index('CCODE')[['CANDINAME', 'ABBR', 'CANDI_ALLIANCE_ID', 'VOTES']]
-    runners = candidates[candidates['#'] == 2].set_index('CCODE')[['CANDINAME', 'ABBR', 'CANDI_ALLIANCE_ID', 'VOTES', 'RNO', 'TIME']]
-    winners.columns = ['WINNER', 'WINNER PARTY', 'WINNER ALLIANCE', 'WINNER VOTES']
-    runners.columns = ['RUNNER', 'RUNNER PARTY', 'RUNNER ALLIANCE', 'RUNNER VOTES', 'FINISHED', 'UPDATED']
+    candidates['CANDI_ALLIANCE_INDIA_ID'].replace({0: 'OTHER', 1: 'NDA', 2: 'UPA', 3: 'OTHER'}, inplace=True)
+    winners = candidates[candidates['#'] == 1].set_index('CCODE')[['CANDINAME', 'ABBR', 'CANDI_ALLIANCE_INDIA_ID', 'VOTES', 'ID']]
+    runners = candidates[candidates['#'] == 2].set_index('CCODE')[['CANDINAME', 'ABBR', 'CANDI_ALLIANCE_INDIA_ID', 'VOTES', 'RNO']]
+    winners.columns = ['WINNER', 'WINNER PARTY', 'WINNER ALLIANCE', 'WINNER VOTES', 'ID']
+    runners.columns = ['RUNNER', 'RUNNER PARTY', 'RUNNER ALLIANCE', 'RUNNER VOTES', 'FINISHED']
 
     # Save the results
     elections = pd.concat([
-        constituencies[['CONST_NAME_LANG2', 'CONST_NAME_LANG1']],
         winners,
         runners], axis=1)
-    elections = elections.reset_index().rename(columns={
-        'CONST_NAME_LANG2': 'STATE',
-        'CONST_NAME_LANG1': 'PC',
-        'index': 'CONST_ID',
-    })
-    elections.to_csv('2014-constituency.csv'.format(args.time * 100), index=False, float_format='%.0f')
+    elections = elections.reset_index().rename(columns={'index': 'CONST_ID'})
+    elections['FINISHED'] = elections['FINISHED'].apply(lambda v: 1 if v == '99' else 0)
+
+    elections.to_csv('2014-constituency.txt'.format(args.time * 100), index=False, float_format='%.0f', sep='\t')
+
+    alliances = elections.pivot_table(cols='FINISHED', rows='WINNER ALLIANCE', values='WINNER', aggfunc='count').fillna(0)
+    alliances.rename(columns={0: 'leading', 1: 'won'}, inplace=True)
+    if alliances['won']['NDA'] + alliances['leading']['NDA'] > alliances['won']['UPA'] + alliances['leading']['UPA']:
+        alliances = alliances.reindex(['NDA', 'OTHER', 'UPA'])
+    else:
+        alliances = alliances.reindex(['UPA', 'OTHER', 'NDA'])
+    alliances['all'] = alliances['leading'] + alliances['won']
+    alliances = alliances.reset_index().rename(columns={'index': 'name'})
+    alliances_dict = json.loads(alliances.reset_index().to_json(orient='records'))
+
+    parties = elections.pivot_table(cols='FINISHED', rows=['WINNER ALLIANCE', 'WINNER PARTY'], values='WINNER', aggfunc='count').fillna(0)
+    parties.rename(columns={0: 'leading', 1: 'won'}, inplace=True)
+    parties['all'] = parties['leading'] + parties['won']
+    parties.sort('all', ascending=False, inplace=True)
+    for row in alliances_dict:
+        subset = parties.ix[row['name']].astype(int).reset_index()
+        subset.rename(columns={'WINNER PARTY': 'name'}, inplace=True)
+        row['parties'] = json.loads(subset.head(5)  .to_json(orient='records'))
+
+    map_data = elections[['ID', 'WINNER PARTY', 'WINNER ALLIANCE', 'WINNER VOTES', 'FINISHED']]
+    map_data = json.loads(map_data.to_json(orient='values'))
 
     # Create JSON structure
     summary = {
-        'updated': candidates['TIME'].max().strftime('%H:%M, %a %d-%b'),
+        'updated': datetime.datetime.now().strftime('%H:%M %p'),
         'constituencies': {
-            'total': len(constituencies),
-            'finished': sum(elections['FINISHED'] == '99'),
-            'counting': sum((elections['FINISHED'] != '99') & (elections['WINNER VOTES'] > 0)),
-            'awaited': sum((elections['FINISHED'] != '99') & (elections['WINNER VOTES'] == 0)),
+            'finished': sum(elections['FINISHED'] == 1),
+            'counting': sum((elections['FINISHED'] != 1) & (elections['WINNER VOTES'] > 0)),
+            'awaited': sum((elections['FINISHED'] != 1) & (elections['WINNER VOTES'] == 0)),
+            'total': len(elections),
         },
-        'alliances': [
-            { 'name': 'NDA',   'won': 5, 'leading': 3},
-            { 'name': 'UPA',   'won': 3, 'leading': 1},
-            { 'name': 'Other', 'won': 1, 'leading': 1},
-        ],
+        'alliances': alliances_dict,
+        'party_maxall': parties['all'].max(),
+        'map': map_data
     }
     with open('2014-summary.json', 'w') as out:
-        json.dump(summary, out)
+        json.dump(summary, out, separators=(',', ':'))
 
 if __name__ == '__main__':
     main()
